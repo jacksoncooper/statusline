9a43b0a7bb786a82955cc5013ee803947fcd8e40
jacksoncooper <jacksoncooper@users.noreply.github.com>
2020-06-02 21:40:06 -0700

    Revise parser data constructor.

--- src/Parse.hs
+++ src/Parse.hs
@@ -4,31 +4,82 @@ module Parse where
 
 import Data.Char
 
-data Result a =
-    Success a
-  | Failure String
-  deriving Show
-
-instance Functor Result where
-  fmap :: (a -> b) -> Result a -> Result b
-  fmap f (Success a) = Success $ f a
-  fmap _ (Failure s) = Failure s
+data State = State
+  { position :: Int
+  , remaining :: String
+  } deriving Show
 
 newtype Parser a =
-  Parser { runParser :: String -> Result (String, a) }
+  Parser { runParser :: State -> Either String (State, a) }
 
 instance Functor Parser where
   fmap :: (a -> b) -> Parser a -> Parser b
-  fmap f (Parser p) =
-    Parser $ (fmap . fmap . fmap) f p
+  fmap f (Parser parser) =
+    Parser $ (fmap . fmap . fmap) f parser
+
+instance Applicative Parser where
+  pure :: a -> Parser a
+  pure a = Parser $ \_ -> Right (State 0 "", a)
+
+  (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+  (Parser parser) <*> (Parser parser') =
+    Parser $ \state ->
+      parser state >>=
+        \(state', f) ->
+          (fmap . fmap) f (parser' state')
+
+instance Monad Parser where
+  return :: a -> Parser a
+  return = pure
+
+  (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+  (Parser parser) >>= f =
+    Parser $ \state ->
+      parser state >>=
+        \(state', a) ->
+          runParser (f a) state'
+
+consumeCharacter :: Parser Char
+consumeCharacter =
+  Parser $ \(State position remaining) ->
+    case remaining of
+      (character : rest) ->
+        Right (State (position + 1) rest, character)
+      "" ->
+        Left "Encountered an empty string."
+
+validateCharacter :: (Char -> Bool) -> (Char -> a) -> (Char -> String) -> Parser a
+validateCharacter isValid transform explanation =
+  -- The parser's monad instance doesn't allow us to fail the parser using a
+  -- bind. We have to operate over the Either type that wraps it. Consider a
+  -- different implementation of the parser's data constructor.
+
+  Parser $ \state ->
+    runParser consumeCharacter state >>=
+      \(state', consumed) ->
+        if isValid consumed
+        then Right (state', transform consumed)
+        else Left (explanation consumed)
+
+parseCharacter :: Char -> Parser Char
+parseCharacter target =
+  let explanation consumed =
+        "The character '"
+        ++ pure consumed
+        ++ "' is not '"
+        ++ pure target
+        ++ "'."
+  in validateCharacter (target ==) id explanation
 
 parseDigit :: Parser Int
 parseDigit =
-  Parser $ \s ->
-    case s of
-      (c:cs) ->
-        if isDigit c
-        then Success (cs, digitToInt c)
-        else Failure $ "The character '" ++ [c] ++ "' is not a digit."
-      [] -> Failure "Encountered and empty string."
+  let explanation consumed =
+        "The character '"
+        ++ pure consumed
+        ++ "' is not a digit."
+  in validateCharacter isDigit digitToInt explanation
+
+-- parseDigits :: Parser [Int]
+-- parseDigits =
+--   Parser $ \s -> undefined
 
