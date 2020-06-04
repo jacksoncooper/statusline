37cab6404e0a00167ff84574da85ed8f4f75570a
jacksoncooper <jacksoncooper@users.noreply.github.com>
2020-06-04 13:35:52 -0700

    Explicitly use line buffering.

--- src/Emit.hs
+++ src/Emit.hs
@@ -4,17 +4,25 @@ module Emit
 
 import Control.Concurrent (threadDelay)
 import Control.Monad (forever)
+import System.IO (stdout,hSetBuffering, BufferMode(LineBuffering))
 
 import Block
 import Blocks.Date
 
--- TODO: Emit JSON as specified by 'swaybar-protocol', and another thread to listen for pause
--- signals.
+-- TODO:
+-- * Emit JSON as specified by 'swaybar-protocol'.
+-- * Add another thread to listen for pause and continue signals.
 
 emit :: IO ()
-emit = forever $
-  getFullText dateBlock >>=
-    \fullText ->
-         putStrLn fullText
-      >> threadDelay (1 * 10 ^ 6)
+emit =
+  let
+    doWrite =
+      getFullText dateBlock >>=
+        \fullText ->
+             putStrLn fullText
+          >> threadDelay (1 * 10 ^ 6)
+  in
+        hSetBuffering stdout LineBuffering
+     >> forever doWrite
+
 
